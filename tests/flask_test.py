15e22de3b893980537bcffcb9aee339743ac4b21
daniel.haimov <daniel.haimov58@gmail.com>
2023-11-05 16:18:23 +0200

    tests init

---
+++ tests/flask_test.py
@@ -0,0 +1,18 @@
+import requests
+
+if __name__ == '__main__':
+    base_url = 'http://localhost:5000'
+    requests.post(url=f'{base_url}/rules', json={
+        "device": "device_name",
+        "sensor_type":  "sensor_name",
+        "operator": "L",
+        "unusual_value": 42,
+        "rule_description": "device_name: sensor higher than 42",
+        "compare_to_last_event": False
+    })
+
+    requests.post(url=f'{base_url}/events', json={
+        "device": "device_name",
+        "sensor_type":  "sensor_name",
+        "sensor_value": 20
+    })
