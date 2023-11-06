5312bbffc285cc41af13eef24f1c57e0db2d698d
daniel.haimov <daniel.haimov58@gmail.com>
2023-11-06 14:09:15 +0200

    add env example

--- tests/DroneSimulator/drone_rules.py
+++ tests/DroneSimulator/drone_rules.py
@@ -2,9 +2,12 @@ import requests
 
 
 def add_rules():
+    requests.post('http://127.0.0.1:5000/rules', json={
+        "device": "drone",
+        "sensor_type":  "height",
+        "operator": "G",
+        "unusual_value": 100,
+        "rule_description": "drone: higher than 100",
+        "compare_to_last_event": False
+    })
 
-    pass
-
-
-def remove_rules():
-    pass
