154ba0dd702419136d50ab62c12f890be38f8341
daniel.haimov <daniel.haimov58@gmail.com>
2023-11-08 15:04:09 +0200

    remove redundant comments

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import os
 import subprocess
 
 if __name__ == '__main__':
-    subprocess.Popen(["python3", "./event_ingestion_microservice.py &"])
+    subprocess.Popen(["python3", "./event_ingestion_microservice.py"])
     subprocess.run(["python3", "./alert_and_notification_microservice.py"])
